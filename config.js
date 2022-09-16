61ddf0b99a7ec1b5eb19beedd0f534bd043538cc
Benedikt Hitz-Gamper <bhitz@gmx.net>
2022-09-16 13:23:18 +0200

    update config.js

--- config.js
+++ config.js
@@ -14,13 +14,13 @@ var respecConfig = {
       name: "Benedikt Hitz-Gamper",
       company: "Bern University of Applied Sciences (BFH)",
       companyURL: "https://www.bfh.ch/",
-      url: "https://www.linkedin.com/in/benedikt-hitz-gamper-331756209/",
-      orcid: "0000-0001-7937-0159",
+      url: "mailto:benedikt.hitz@bfh.ch",
     },
     {
       name: "Michael Luggen",
       company: "Bern University of Applied Sciences (BFH)",
-      companyUrl: "https://www.bfh.ch",
+      companyURL: "https://www.bfh.ch",
+      url: "mailto:michael.luggen@bfh.ch"
     }]
   };
 
